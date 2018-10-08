8faf0a3794cd34185c4da09ad8de8f0ea05cce63
gaodandan <dandan.gao@oceanus-plus.com>
2018-10-08 11:36:36 +0800

    注册登录静态页面实现

--- pat-mall/src/App.vue
+++ pat-mall/src/App.vue
@@ -11,7 +11,6 @@ export default {
 </script>
 
 <style lang="less">
-@pColor:#f65;
 #app {
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
--- pat-mall/src/views/registor.vue
+++ pat-mall/src/views/registor.vue
@@ -2,9 +2,53 @@
 <template>
     <div>
        <div class="registor">
-           <!-- <div class="title">
-               <span>{{logo}}</span><span>用户注册</span>
-           </div> -->
+         <div class="cont">
+           <Tabs value="name1">
+               <TabPane label="注册" name="name1">
+                  <Form ref="formInline" :model="formInline" :rules="ruleInline">
+                    <FormItem prop="user">
+                        <Input type="text" v-model="formInline.user" placeholder="请输入用户名">
+                            <Icon type="ios-person-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem prop="password">
+                        <Input type="password" v-model="formInline.password" placeholder="请输入密码">
+                            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem prop="confirmPass">
+                        <Input type="password" v-model="formInline.confirmPass" placeholder="请再次输入密码">
+                            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem style="text-align:center">
+                        <Button type="primary" @click="handleSubmit('formInline')" >注册</Button>
+                    </FormItem>
+                  </Form>
+               </TabPane>
+               <TabPane label="登录" name="name2">
+                 <Form ref="formInline" :model="formInline" :rules="ruleInline">
+                    <FormItem prop="user">
+                        <Input type="text" v-model="formInline.user" placeholder="请输入用户名">
+                            <Icon type="ios-person-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem prop="password">
+                        <Input type="password" v-model="formInline.password" placeholder="请输入密码">
+                            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem style="text-align:center">
+                        <Button type="primary" @click="handleSubmit('formInline')" >登录</Button>
+                    </FormItem>
+                  </Form>
+               </TabPane>
+              
+           </Tabs>
+          
+         </div>
+          
+
        </div>
     </div>
 </template>
@@ -14,7 +58,18 @@ import headTop from "@/components/headTop.vue";
 export default {
   data() {
     return {
-      logo: "logo"
+      logo: "logo",
+      formInline: {
+        user: "",
+        password: ""
+      },
+      ruleInline: {
+        user: [{ required: true, message: "请填写用户名" }],
+        password: [
+          { required: true, message: "请填写密码" },
+          { type: "string", min: 6, message: "密码长度应超过6位" }
+        ]
+      }
     };
   },
   components: {
@@ -24,10 +79,7 @@ export default {
 </script>
 <style lang="less" scoped>
 @import "~@/assets/styles/variables.less";
-.bg {
-  background: @bgColor;
-  color: @baseColor;
-}
+@import "~@/assets/styles/style.less";
 </style>
 
 
